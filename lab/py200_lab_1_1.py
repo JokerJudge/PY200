4735e22585275c97788573a5b1cb5d7e77b452d3
MiNote <kovalenko_evgeny@bk.ru>
2020-02-21 00:12:33 +0300

    lab_1_1 is done

--- lab/py200_lab_1_1.py
+++ lab/py200_lab_1_1.py
@@ -372,6 +372,8 @@ class Node:
     def set_value(self, data):
         self.data = data
 
+    # c перегруженными __str__ и __repr__ постоянно при обращении в классе Linkedlist к экземпляру Node выдавал RecursionError
+
     #def __str__(self):
     #    return f"Node: {self.data}, Next node: {self.next_}, Previous node: {self.prev}"
 
@@ -383,7 +385,7 @@ class LinkedList:
         if nodes is None:
             self.head = None
             self.tail = None
-        elif isinstance(nodes, Node): # Один нод - можно только типы проверять. Это будет значить, что тут один нод был передан
+        elif isinstance(nodes, Node):
             self.head = nodes # указываем, что это тот же нод
             self.tail = nodes # указываем, что это тот же нод
         elif isinstance(nodes, list):
@@ -399,13 +401,26 @@ class LinkedList:
                 for i in nodes:
                     if not isinstance(i, Node):
                         raise TypeError("Передаваться должен Node или список из Node")
-                self.head = nodes[0] # нужно обязательно проверять на вводимое значение (если там будут не только ноды - то должна быть ошибка)
+                self.head = nodes[0]
                 self.tail = nodes[-1]
-                #TODO
                 self.linked_nodes(nodes) # связываем Node в порядке подачи в списке
 
         else:
             raise TypeError("Передаваться должен Node или список из Node")
+
+    def __len__(self): # считаем количество Node в Linkedlist
+        if self.head is None and self.tail is None:
+            return 0
+        elif self.head == self.tail:
+            return 1
+        else:
+            current_node = self.head
+            current_node = current_node.next_
+            index = 1 # счетчик
+            while current_node != self.head:
+                index += 1
+                current_node = current_node.next_
+            return index
 
     def linked_nodes(self, nodes):
         # установили ссылки для первого нода
@@ -428,24 +443,23 @@ class LinkedList:
         index - position of node
         '''
         #находим нужный Node
+        if not isinstance(node, Node):
+            raise TypeError("В node был передан не Node")
         if index == 0:
             self.append_left(node)
-        elif index > 0: # пока не понятно, как ограничить возможный индекс длиной Linkedlist + нужна проверка на int
-            #TODO нужно реализовать __len__ в Linkedlist
+        elif isinstance(index, int) and 0 < index < len(self): # проверяем, что в index записано число, и что это число не больше длины списка
             i = 0 # счетчик
             current_node = self.head
-            #lst = []
-            #lst.append(self.head)
-            print(node)
             while i != index:
                 i += 1
                 current_node = current_node.next_
-                print(current_node)
-            # устанавливаем связи
-            current_node.prev.set_next = node
-            node.set_prev = current_node.prev
-            current_node.set_prev = node
-            node.set_next = current_node
+            # устанавливаем связи, вставляя Node на нужное место
+            current_node.prev.set_next(node)
+            node.set_prev(current_node.prev)
+            current_node.set_prev(node)
+            node.set_next(current_node)
+        else:
+            raise ValueError("Такого количества Node нет в Linkedlist либо в index указано не число")
 
     def append(self, node):
         '''
@@ -453,6 +467,11 @@ class LinkedList:
         :param node: Node
         :return:
         '''
+        if not isinstance(node, Node):
+            raise TypeError("В node был передан не Node")
+        if self.head is None and self.tail is None:
+            self.head = node
+            self.tail = node
         self.tail.set_next(node)
         node.set_prev(self.tail)
         self.tail = node
@@ -465,6 +484,8 @@ class LinkedList:
         Append Node to the beginning of LinkedList
         node - Node
         '''
+        if not isinstance(node, Node):
+            raise TypeError("В node был передан не Node")
         self.head.set_prev(node)
         node.set_next(self.head)
         self.head = node
@@ -475,49 +496,124 @@ class LinkedList:
         '''
         Clear LinkedList
         '''
-        ...
+        self.head.set_next(None)
+        self.head.set_prev(None)
+        self.tail.set_next(None)
+        self.tail.set_prev(None)
+        self.head = None
+        self.tail = None
+        # через удаление Node не получилось. Все равно почему-то Node внутри списка не удалялись.
+        # сделал в итоге через установкой всех атрибутов на None, чтобы сбились ссылки.
+        '''
+        # current_node = self.head
+        # for i in range(len(self)-1):
+        #     current_node = current_node.next_
+        #     print(current_node, current_node.data)
+        #     del current_node.prev
+        # del current_node
+        # print(self.head, self.head.data)
+        '''
 
     def find(self, node):
-        ...
-
+        '''
+        Поиск по Node
+        :param node:
+        :return: Если находит - возвращает индекс в списке, если не находит - None
+        '''
+        if not isinstance(node, Node):
+            raise TypeError("В node был передан не Node")
+        current_node = self.head
+        for i in range(len(self)):
+            if node == current_node:
+                return i
+            current_node = current_node.next_
+        return None
 
     def remove(self, node):
-        ...
+        '''
+        Удаление по Node
+        :param node:
+        :return:
+        '''
+        if not isinstance(node, Node):
+            raise TypeError("В node был передан не Node")
+        current_node = self.head
+        for i in range(len(self)):
+            if node == current_node:
+                current_node.prev.set_next(current_node.next_)
+                current_node.next_.set_prev(current_node.prev)
+                return
+            current_node = current_node.next_
+        print(f"{node} не был найден в {self}")
+
         
     def delete(self, index):
-        ...
+        """
+        Удаление node по индексу
+        :param index: индекс нода
+        :return:
+        """
+        if not isinstance(index, int):
+            raise TypeError("Индекс должен быть числом")
+        if 0 < index < len(self):
+            current_node = self.head
+            for i in range(len(self)):
+                if index == i:
+                    current_node.prev.set_next(current_node.next_)
+                    current_node.next_.set_prev(current_node.prev)
+                    return
+                current_node = current_node.next_
+        else:
+            raise ValueError("Индекс за пределами возможных значений")
 
+if __name__ == "__main__":
+    # создадим 8 экземпляров Node
+    a1 = Node(data=10)
+    a2 = Node(data=35)
+    a3 = Node(data=75)
+    a4 = Node(data=115)
+    a5 = Node(data=5)
+    a6 = Node(data=125)
+    a7 = Node(data=53)
+    a8 = Node(data=143)
 
-a1 = Node(data=10)
-a2 = Node(data=35)
-a3 = Node(data=75)
-a4 = Node(data=115)
-a5 = Node(data=5)
-a6 = Node(data=125)
-a7 = Node(data=53)
+    # создадим 2 двусвязных списка
+    l1 = LinkedList(a1)
+    l2 = LinkedList([a1, a2, a3])
 
-print(a7)
+    l2.append(a4) # добавим в список а4
+    l2.append_left(a5) # добавим а5 в начало списка
+    l2.append(a6) # добавим в конец а6
+    l2.insert(a7, index=2) # вставим на место a2 (индекс 2) вставим a7. Теперь последовательность a1 - a7 - a2
+    print(l2.head.next_.next_.data) # удостоверимся, что третьим с начала элементом будет a7
+    print("Длина списка l2: ", len(l2)) # проверим работу __len__ - должно быть 7
+    print(l2) # смотрим на область памяти списка l2
+    l2.clear() # очищение списка
+    print(l2) # проверяем, что это тот же список
+    print(l2.head) # должно быть None
+    print(l2.tail) # должно быть None
+    print(len(l2)) # длина должна быть 0
+    print("------------------------")
+    l2.append(a1) # добавим новый элемент в пустой список
+    print(l2.head) # head и tail должны быть одинаковыми
+    print(l2.head.next_) # переходит сам на себя.
+    print(len(l2)) # длина должна быть 1
+    print("------------------------")
 
-l1 = LinkedList(a1)
-l2 = LinkedList([a1, a2, a3])
-print(l1)
-print(l2)
-print(dir(l2))
-print(l2.tail)
-l2.append(a4)
-l2.append_left(a5)
-l2.append(a6)
-print(l2.tail.next_.next_.data)
-l2.insert(a7, 2)
-print(l2.head.next_.next_.next_.data)
-
-
-
-
-
-
-
-
+    l3 = LinkedList([a1, a2, a3, a4, a5, a6, a7]) # создаем новый список
+    print("Индекс искомого элемента: ", l3.find(a5)) # проверяем работу метода find
+    print("Индекс искомого элемента: ", l3.find(a8))  # проверяем работу метода find
+    l3.remove(a8) # попытка удалить отсутствующий Node
+    print("Длина списка до удаления: ", len(l3))
+    l3.remove(a2)
+    print("Длина списка после удаления: ", len(l3))
+    print("Индекс Node a3: ", l3.find(a3))
+    print("Индекс Node a2: ", l3.find(a2))
+    print("Длина списка до удаления: ", len(l3))
+    l3.delete(1)
+    print("Длина списка после удаления: ", len(l3))
+    print("Индекс Node a3: ", l3.find(a3))
+    print("Индекс Node a6: ", l3.find(a6))
 
 
 
